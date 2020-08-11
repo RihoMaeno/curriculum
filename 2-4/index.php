ff6d8502b0e57aa2346927b84c1f72f9d51c053d
RihoMaeno <69238582+RihoMaeno@users.noreply.github.com>
2020-08-11 15:57:11 +0900

    2-4

---
+++ 2-4/index.php
@@ -0,0 +1,24 @@
+<?php
+echo 1+1;
+echo '<br>';
+echo 2-1;
+echo '<br>';
+echo 2*2;
+echo '<br>';
+echo 6/2;
+echo '<br>';
+echo 3%2;
+echo '<br>';
+$x=2;
+$x++; // $x = $x + 1
+echo $x;
+echo '<br>';
+$x--;// $x = $x - 1
+echo $x;
+echo '<br>';
+$x += 5;// $x = $x + 5;
+echo $x;
+echo '<br>';
+$x *= 5; // $x = $x * 5;
+echo $x;
+?>
