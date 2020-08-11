7809bf3aabeabb52f04d126f0e7404c0d74c012f
RihoMaeno <69238582+RihoMaeno@users.noreply.github.com>
2020-08-11 15:55:41 +0900

    2-2

---
+++ 2-2/index.php
@@ -0,0 +1,24 @@
+<?php
+// index.php
+$message = "Hello World!";
+var_dump($message);
+
+echo '<br>';
+$x = 8;
+$y = 5.55;
+echo $x;
+// 8を出力した後に改行
+echo '<br>';
+echo $y."<br>";
+
+$emp = null;
+var_dump($emp);
+echo '<br>';
+
+$en_words = 'Hello World';
+var_dump($en_words);
+echo '<br>';
+$ja_words = 'こんにちは';
+var_dump($ja_words);
+echo '<br>';
+?>
