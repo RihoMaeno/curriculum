51af8f6b5a99b50bef65b2f84da6a4141a5c38fa
RihoMaeno <69238582+RihoMaeno@users.noreply.github.com>
2020-08-13 10:15:11 +0900

    2-5

--- 2-5/If.php
+++ 2-5/If.php
@@ -1,11 +1,11 @@
 <?php
 $name = "taro";
 $password = "pass";
-if ($name="taro" && $pass="pass" ){
+if ($name=="taro" && $password=="pass" ){
     echo "ログイン成功です";
-} elseif($name=taro) {
+} elseif($name=="taro") {
     echo "パスワードが間違っています。";
-}elseif($pass=pass){
+}elseif($password=="pass"){
     echo "名前が間違っています。";
 }else {
     echo "入力情報が間違っています。";
